d15281f530d7e363c324a88bf38a91d8dc934f07
mike moores <michaelmoores@hotmail.com>
2023-11-18 20:22:08 +1300

    tidying code

--- server/redux/engineSlice.tsx
+++ server/redux/engineSlice.tsx
@@ -2,10 +2,6 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   play: false,
-  die: false,
-  speed: 0,
-  score: 0,
-  lastScore: 0,
   loadingScreen: true,
 }
 
@@ -16,18 +12,6 @@ export const engineSlice = createSlice({
     setReady: (state, action) => {
       state.play = action.payload
     },
-    setDie: (state, action) => {
-      state.die = action.payload
-    },
-    setSpeed: (state, action) => {
-      state.speed += action.payload
-    },
-    setScore: (state, action) => {
-      state.score = action.payload
-    },
-    setLastScore: (state, action) => {
-      state.lastScore = action.payload
-    },
     setLoadingScreen: (state, action) => {
       state.loadingScreen = action.payload
     },
@@ -36,10 +20,7 @@ export const engineSlice = createSlice({
 
 export const {
   setReady,
-  setDie,
-  setSpeed,
-  setScore,
-  setLastScore,
+
   setLoadingScreen,
 } = engineSlice.actions
 export default engineSlice.reducer
